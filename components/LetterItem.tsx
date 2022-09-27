5790144cd7bf5be07db2a292f0249d922ad470f7
somin <smhs12@naver.com>
2022-09-27 16:40:42 +0900

    Design: PERFUME STORY 페이지 Letter 모달창 레이아웃

--- components/LetterItem.tsx
+++ components/LetterItem.tsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/no-unknown-property */
 import type { NextPage } from "next";
+import { PropsWithChildren, useState, useCallback } from "react";
+import LetterItemModal from "./LetterItemModal";
 
 const LetterItem: NextPage = () => {
+  const [isModalOpen, setIsOpenModal] = useState<boolean>(false);
+  const onClickToggleModal = useCallback(() => {
+    setIsOpenModal(!isModalOpen);
+    console.log(isModalOpen);
+  }, [isModalOpen]);
   return (
-    <div className="item-container">
+    <div className="item-container" onClick={onClickToggleModal}>
       <img src="/letterItemImg.png" />
       <div className="item-title">킹받드라슈의 일기</div>
+      {isModalOpen && (
+        <LetterItemModal onClickToggleModal={onClickToggleModal} />
+      )}
       <style jsx>{`
         .item-container {
           margin: 1rem 0;
