8ae2b44165dcba8c0bbe5f79a8c566be3b255b73
kjyong702 <kjyong702@naver.com>
2022-10-09 16:36:06 +0900

    prisma schema 수정

---
+++ pages/api/users/signup.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export default async function signup(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { name, email, phoneNumber } = req.body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (existingUser) {
+    res.status(400).json({
+      message: "User already exists",
+    });
+  }
+
+  const password = await bcrypt.hash(req.body.password, 10);
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      password,
+      phoneNumber,
+    },
+  });
+
+  return res.status(200).json(user);
+}
