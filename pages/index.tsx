50a675603fa686b0097439fe23da0c8e1a83cbd0
somin <smhs12@naver.com>
2022-10-19 16:04:07 +0900

    Design: Home 레이아웃

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import styled from "styled-components";
-import styles from "../styles/Home.module.css";
+import { BsFillTriangleFill } from "react-icons/bs";
 
 const Home: NextPage = () => {
   return (
@@ -48,7 +46,84 @@ const Home: NextPage = () => {
           <button>Read More {`>`}</button>
         </div>
       </ThirdMain>
-      <div></div>
+      <FourthMain>
+        <FourthFirst>
+          <div className="perfume-list">
+            <div className="perfume-item">
+              <img src="/perNoteBackImg.png" alt="image" />
+              <div>Lorem Ipsum</div>
+            </div>
+            <div className="perfume-item">
+              <img src="/perNoteBackImg.png" alt="image" />
+              <div>Lorem Ipsum</div>
+            </div>
+            <div className="perfume-item">
+              <img src="/perNoteBackImg.png" alt="image" />
+              <div>Lorem Ipsum</div>
+            </div>
+          </div>
+          <div className="perfume-story-text">
+            <div className="perfume-story-text-title">Purfume story</div>
+            <div className="perfume-story-text-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
+              gravida sit facilisis cras. In blandit mi id est luctus. Non
+              <br />
+              turpis non risus purus. Eget vel commodo ac purus, laoreet
+              sollicitudin. <br />
+              <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Massa gravida sit facilisis cras.
+              <br /> In blandit mi id est luctus. Non turpis non risus purus.
+              Eget vel commodo ac purus, laoreet sollicitudin.
+            </div>
+          </div>
+          <button>
+            <span>Subscribe</span>
+            <span className="subscribe-icon">
+              <BsFillTriangleFill
+                style={{ transform: "rotate(90deg)", color: "#A79D9D" }}
+              />
+            </span>
+          </button>
+        </FourthFirst>
+        <FourthSecond>
+          <div className="description">
+            <div className="description-title">노트,성격,특징,브랜드 통합</div>
+            <div className="description-content">
+              Lorem ipsum dolor sit amet, consectetur <br />
+              adipiscing elit. Massa gravida sit facilisis
+              <br /> cras. In blandit mi id est luctus. Non <br />
+              turpis non risus purus. Eget vel commodo <br />
+              ac purus, laoreet sollicitudin.
+            </div>
+            <button>Read More {`>`}</button>
+          </div>
+          <div className="image-container">
+            <img src="/perNoteBackImg.png" alt="image1" />
+            <img src="/perNoteBackImg.png" alt="image2" />
+          </div>
+        </FourthSecond>
+        <FourthThird>
+          <img src="/perNoteBackImg.png" alt="second main image" />
+          <div className="description">
+            <div className="description-title">설명글~~~</div>
+            <div className="description-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <br />
+              Consectetur sed in urna risus aliquet massa consectetur
+              <br /> mattis. Arcu et diam tortor quis pulvinar vel dolor
+              <br /> condimentum. Donec id eu porttitor turpis enim dui.
+              <br /> Faucibus eu blandit hac morbi mi. Tincidunt sagittis amet{" "}
+              <br />
+              placerat lacus, varius feugiat. Tempor sed orci adipiscing
+              <br /> molestie ipsum eu sed semper. Viverra rutrum ornare ut sit{" "}
+              <br />
+              euismod molestie turpis. Massa risus, quisque non viverra <br />
+              nam consequat cursus consectetur.
+            </div>
+            <button>Read More {`>`}</button>
+          </div>
+        </FourthThird>
+      </FourthMain>
     </HomeContainer>
   );
 };
@@ -96,6 +171,7 @@ const SecondMain = styled.div`
     width: 1668px;
     height: 392px;
     margin-bottom: 56px;
+    border-radius: 30px;
   }
 
   .description {
@@ -119,6 +195,7 @@ const SecondMain = styled.div`
 const ThirdMain = styled.div`
   padding: 180px 0;
   padding-left: 174px;
+  margin-bottom: 209px;
   background-color: #eaeaea;
   display: flex;
 
@@ -126,6 +203,7 @@ const ThirdMain = styled.div`
     width: 680px;
     height: 720px;
     margin-right: 106px;
+    border-radius: 30px;
   }
   .description {
     display: flex;
@@ -149,6 +227,150 @@ const ThirdMain = styled.div`
       border: none;
       border-radius: 100px;
       background-color: #ffffff;
+      font-weight: normal;
+      font-size: 20px;
+    }
+  }
+`;
+
+const FourthMain = styled.div`
+  padding-left: 131px;
+`;
+
+const FourthFirst = styled.div`
+  margin-bottom: 230px;
+  .perfume-list {
+    margin-bottom: 60px;
+    display: flex;
+
+    .perfume-item {
+      margin-right: 34px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      img {
+        width: 530px;
+        height: 530px;
+        border-radius: 30px;
+        margin-bottom: 30px;
+      }
+
+      div {
+        font-weight: normal;
+        font-size: 30px;
+      }
+    }
+  }
+
+  .perfume-story-text {
+    .perfume-story-text-title {
+      margin-bottom: 25px;
+      font-weight: bold;
+      font-size: 50px;
+    }
+
+    .perfume-story-text-content {
+      margin-bottom: 35px;
+      font-weight: normal;
+      font-size: 30px;
+    }
+  }
+
+  button {
+    width: 582px;
+    height: 119px;
+
+    padding: 0 37px;
+    background-color: #d9d9d9;
+
+    border: none;
+    border-radius: 100px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    span {
+      font-weight: normal;
+      font-size: 40px;
+    }
+  }
+`;
+
+const FourthSecond = styled.div`
+  display: flex;
+  margin-bottom: 338px;
+  .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-right: 145px;
+
+    .description-title {
+      font-weight: bold;
+      font-size: 50px;
+      margin-bottom: 30px;
+    }
+
+    .description-content {
+      font-weight: normal;
+      font-size: 30px;
+      margin-bottom: 35px;
+    }
+    button {
+      width: 166px;
+      padding: 12px 23.5px;
+      border: none;
+      border-radius: 100px;
+      background-color: #eaeaea;
+      font-weight: normal;
+      font-size: 20px;
+    }
+  }
+
+  .image-container {
+    img {
+      width: 444px;
+      height: 642px;
+      margin-right: 74px;
+      border-radius: 30px;
+    }
+  }
+`;
+
+const FourthThird = styled.div`
+  display: flex;
+  margin-bottom: 30rem;
+
+  img {
+    width: 680px;
+    height: 720px;
+    margin-right: 106px;
+    border-radius: 30px;
+  }
+  .description {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    .description-title {
+      margin-bottom: 1.563rem;
+      font-weight: bold;
+      font-size: 50px;
+    }
+
+    .description-content {
+      margin-bottom: 35px;
+      font-weight: normal;
+      font-size: 30px;
+    }
+
+    button {
+      width: 166px;
+      padding: 12px 23.5px;
+      border: none;
+      border-radius: 100px;
+      background-color: #eaeaea;
       font-weight: normal;
       font-size: 20px;
     }
