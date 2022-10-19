e8fcb925361f145fe5b1cb853728ca4ae3402075
bswebdvlpr <tfl0324@gachon.ac.kr>
2022-10-19 14:32:21 +0900

    commit for pull

--- pages/backend/test.tsx
+++ pages/backend/test.tsx
@@ -3,15 +3,24 @@ import type { NextPage } from "next";
 const test: NextPage = ({results}) => {
     return (
         <div>
+            {results}
         </div>
     );
 }
 
 export default test;
 
+/*
+Server Error
+Error: Error serializing `.results` returned from `getServerSideProps` in "/backend/test".
+Reason: `undefined` cannot be serialized as JSON. Please use `null` or omit this value.
+
+This error happened while generating the page. Any console logs will be displayed in the terminal window.
+*/
+
 export async function getServerSideProps(){
     const {results} = await (await fetch(
-        'http://localhost:3005/api/db'
+        'http://localhost:3005/api/db/personalScent'
     )).json();
     
     return {
