0b21489e5bb26567d8a4b1e37c8724958904da2d
somin <smhs12@naver.com>
2022-12-13 17:47:43 +0900

    feat: api 경로 수정

--- pages/signin/googleHandler.tsx
+++ pages/signin/googleHandler.tsx
@@ -6,9 +6,11 @@ function GoogleHandler() {
     const parsedHash = new URLSearchParams(window.location.hash.substring(1));
     const access_Token = parsedHash.get("access_token");
     console.log(access_Token);
-    const response = axios.post("/api/auth/googleLogin.ts", {
+    const response = axios.post("/api/auth/googleLogin", {
       accessToken: access_Token,
     });
+    // const { user, accessToken, refreshToken } = response;
   }, []);
+  return <div></div>;
 }
 export default GoogleHandler;
