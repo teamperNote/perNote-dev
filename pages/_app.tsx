ebbfff70e0073c1a3b50efc23591cfd7aa5f657a
somin <smhs12@naver.com>
2022-12-17 17:38:02 +0900

    feat: react-query 초기 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,20 @@ import Layout from "../components/Layout";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/global-styles";
 import { theme } from "../styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
