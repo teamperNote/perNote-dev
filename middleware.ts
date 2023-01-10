49061aebc86766499b6ebcf2b54a2817c6384cc1
kjyong702 <kjyong702@naver.com>
2023-01-10 17:48:27 +0900

    jwt 미들웨어 수정

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,6 @@ const secretKey = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
 
 export async function middleware(req: NextRequest) {
   const role = req.headers.get("authorization");
-
   if (!role) {
     return NextResponse.redirect(
       new URL("/api/middleware/unauthorized", req.url),
@@ -13,11 +12,15 @@ export async function middleware(req: NextRequest) {
   }
 
   const accessToken = role.split("Bearer ")[1];
+  if (!accessToken) {
+    return NextResponse.redirect(
+      new URL("/api/middleware/unauthorized", req.url),
+    );
+  }
 
   try {
-    const { payload } = await jwtVerify(accessToken, secretKey);
-    console.log(payload.iss);
-  } catch (error) {
+    await jwtVerify(accessToken, secretKey);
+  } catch (e) {
     return NextResponse.redirect(
       new URL("/api/middleware/tokenExpired", req.url),
     );
