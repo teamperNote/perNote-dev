6a29ccedc1bb9f15d64ad0dbff4be649bf9f07b1
kjyong702 <kjyong702@naver.com>
2023-01-20 20:40:18 +0900

    Feat: perfumeStory API 구조 변경

--- pages/api/story/index.ts
+++ pages/api/story/index.ts
@@ -1,5 +1,3 @@
-// 유저의 좋아요 여부를 포함한 전체 perfumeStory 반환
-
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 
@@ -9,39 +7,40 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const userId = req.query.userId as string;
+  if (req.method === "POST") {
+    const { title, body, tags } = req.body;
 
-  const isLiked = [];
-  const allStoryIdForUser = [];
-  const allStories = await prisma.story.findMany();
-
-  // 1. 비로그인 시 바로 전체 perfumeStory 반환
-  if (!userId) {
-    allStories.forEach((data) => {
-      isLiked.push(Object.assign(data, { liked: false }));
-    });
-  }
-
-  // 2. 로그인 시 해당 유저의 좋아여 여부를 포함한 전체 perfumeStory 반환
-  else {
-    const storyLikesForUser = await prisma.storyLike.findMany({
-      where: { userId },
-      include: {
-        story: true,
+    const createdPost = await prisma.story.create({
+      data: {
+        title,
+        body,
+        tags,
       },
     });
-    storyLikesForUser.forEach((data) => {
-      allStoryIdForUser.push(data.story.id);
-    });
 
-    allStories.forEach((data) => {
-      if (allStoryIdForUser.includes(data.id)) {
-        isLiked.push(Object.assign(data, { liked: true }));
-      } else {
-        isLiked.push(Object.assign(data, { liked: false }));
-      }
-    });
+    return res.status(200).json(createdPost);
   }
+  if (req.method === "GET") {
+    const storyId = req.query.storyId as string;
 
-  return res.status(200).json(isLiked);
+    const clickedStory = await prisma.story.update({
+      where: { id: storyId },
+      data: {
+        viewCount: {
+          increment: 1,
+        },
+      },
+    });
+
+    return res.status(200).json(clickedStory);
+  }
+  if (req.method === "DELETE") {
+    const storyId = req.query.storyId as string;
+
+    const deletedStory = await prisma.story.delete({
+      where: { id: storyId },
+    });
+
+    return res.status(200).json(deletedStory);
+  }
 }
