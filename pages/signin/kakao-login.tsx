560dd0bff372e5e081a32ca93e93cb1532ead096
somin <smhs12@naver.com>
2023-02-13 22:26:25 +0900

    style: 소셜로그인 로고 추가(구글 제외)

--- pages/signin/kakao-login.tsx
+++ pages/signin/kakao-login.tsx
@@ -7,7 +7,9 @@ function KaKaoLogin() {
   const api_url = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=talk_message`;
   return (
     <>
-      <a href={api_url}>kakao</a>
+      <a href={api_url}>
+        <img src="/login_kakao.svg" alt="카카오로 로그인" />
+      </a>
     </>
   );
 }
