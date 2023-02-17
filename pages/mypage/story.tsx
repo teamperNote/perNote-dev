b09489a99b84e52b8f0001baeb5be155947b4624
somin <smhs12@naver.com>
2023-02-17 13:52:33 +0900

    style: 찜한 스토리 페이지 레이아웃

---
+++ pages/mypage/story.tsx
@@ -0,0 +1,46 @@
+import RowStoryCard from "components/story/RowStoryCard";
+import React, { useState } from "react";
+import styled from "styled-components";
+import SortDropDown from "components/category/SortDropDown";
+import { sortArray } from "lib/modules";
+function Story() {
+  const [sort, setSort] = useState(sortArray[0].value);
+
+  return (
+    <StoryContainer>
+      <StoryTitle>찜한 스토리</StoryTitle>
+
+      <SortBox>
+        <SortDropDown sort={sort} setSort={setSort} />
+      </SortBox>
+      <StoryList>
+        <StoryItem></StoryItem>
+      </StoryList>
+    </StoryContainer>
+  );
+}
+
+export default Story;
+
+const StoryContainer = styled.main`
+  padding-top: 290px;
+  max-width: 1420px;
+  margin: 0 auto;
+`;
+
+const StoryTitle = styled.h2`
+  margin: 0;
+  margin-bottom: 110px;
+  font-weight: 700;
+  font-size: 50px;
+`;
+const SortBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+const StoryList = styled.ul`
+  padding: 0;
+  list-style-type: none;
+`;
+
+const StoryItem = styled.li``;
