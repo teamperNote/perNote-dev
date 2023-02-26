8470ea507c9e75f4e433e254e8483eea7d070ad3
kjyong702 <kjyong702@naver.com>
2023-02-26 14:34:36 +0900

    인기 스토리 반환  개수 변경

--- middleware.ts
+++ middleware.ts
@@ -4,28 +4,28 @@ import { jwtVerify } from "jose";
 const secretKey = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
 
 export async function middleware(req: NextRequest) {
-  const role = req.headers.get("Authorization");
-  if (!role) {
-    return NextResponse.redirect(
-      new URL("/api/middleware/unauthorized", req.url),
-    );
-  }
+  // const role = req.headers.get("Authorization");
+  // if (!role) {
+  //   return NextResponse.redirect(
+  //     new URL("/api/middleware/unauthorized", req.url),
+  //   );
+  // }
 
-  const accessToken = role.split("Bearer ")[1];
-  console.log(accessToken);
+  // const accessToken = role.split("Bearer ")[1];
+  // console.log(accessToken);
 
-  try {
-    const { payload } = await jwtVerify(accessToken, secretKey);
-    console.log(payload.iss);
-  } catch (error) {
-    return NextResponse.redirect(
-      new URL("/api/middleware/tokenExpired", req.url),
-    );
-  }
+  // try {
+  //   const { payload } = await jwtVerify(accessToken, secretKey);
+  //   console.log(payload.iss);
+  // } catch (error) {
+  //   return NextResponse.redirect(
+  //     new URL("/api/middleware/tokenExpired", req.url),
+  //   );
+  // }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/api/users/checkEmail"],
+  matcher: "",
 };
