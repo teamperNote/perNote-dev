85872429cd2b10c7742ac12067ba8097b1b451c6
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-03-02 14:43:01 +0900

    Modify: brandList - comlun select

--- pages/api/category/brandList.ts
+++ pages/api/category/brandList.ts
@@ -1,37 +1,45 @@
-import { PrismaClient } from '@prisma/client';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse){
-    const dict = {}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const dict = {};
 
-    for(let i=65; i<=90; i++){
-        dict[String.fromCharCode(i)] = []
-    }
+  for (let i = 65; i <= 90; i++) {
+    dict[String.fromCharCode(i)] = [];
+  }
 
-    const brands = await prisma.brand.findMany({
-        orderBy: {
-            name_eng: 'asc'
-        }
-    })
-    if(!brands) {
-        return res.status(200).json({
-            message: "Error: /category/brand"
-        })
-    }
-
-    for(const brand of brands){
-        const head = brand.name_eng[0].toUpperCase() // 추후 upperCase 제거
-        if(!dict[head]){
-            return res.status(404).json({
-                message: "Error: /category/brand - wrong data"
-            })
-        }
-        dict[head].push(brand)
-    }
-
+  const brands = await prisma.brand.findMany({
+    select: {
+      id: true,
+      name_eng: true,
+      imgUrl: true,
+    },
+    orderBy: {
+      name_eng: "asc",
+    },
+  });
+  if (!brands) {
     return res.status(200).json({
-        dict
-    })
+      message: "Error: /category/brand",
+    });
+  }
+
+  for (const brand of brands) {
+    const head = brand.name_eng[0].toUpperCase(); // 추후 upperCase 제거
+    if (!dict[head]) {
+      return res.status(404).json({
+        message: "Error: /category/brand - wrong data",
+      });
+    }
+    dict[head].push(brand);
+  }
+
+  return res.status(200).json({
+    dict,
+  });
 }
