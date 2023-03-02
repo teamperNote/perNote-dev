13a59c280e289a50de9ead08e97610afadbd1701
kjyong702 <kjyong702@naver.com>
2023-03-02 16:16:36 +0900

    fix: 카카오 회원가입 중복 처리

--- pages/api/auth/kakao/signup.ts
+++ pages/api/auth/kakao/signup.ts
@@ -13,19 +13,44 @@ export default async function handler(
       req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-        phoneNumber,
-        birth,
-        gender,
-        snsId: userId,
-        snsType: "kakao",
-      },
-    });
 
-    return res.status(200).json(user);
+    const user = await prisma.user.findUnique({
+      where: { phoneNumber },
+    });
+    // 1. 기존에 가입한 정보가 있는 경우
+    if (user) {
+      const updatedUser = await prisma.user.update({
+        where: { phoneNumber },
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+          phoneNumber,
+          birth,
+          gender,
+          snsId: userId,
+          snsType: "kakao",
+        },
+      });
+
+      return res.status(200).json(updatedUser);
+    }
+    // 2. 기존에 가입된 정보가 없는 경우
+    else {
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+          phoneNumber,
+          birth,
+          gender,
+          snsId: userId,
+          snsType: "kakao",
+        },
+      });
+
+      return res.status(200).json(user);
+    }
   }
 }
