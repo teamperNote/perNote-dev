d13ccb7ab55fd193f0cd3900d904c389643e01f7
kjyong702 <kjyong702@naver.com>
2023-03-03 16:32:29 +0900

    feat: 인기 스토리 좋아요 표시 추가

--- pages/api/story/best.ts
+++ pages/api/story/best.ts
@@ -1,12 +1,15 @@
 // 인기 스토리 반환 기능 - 좋아요 개수로 선정
 
 import type { NextApiRequest, NextApiResponse } from "next";
+import { TRUE } from "sass";
 import prisma from "../../../prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const { userId } = req.body;
+
   const allStories = await prisma.story.findMany({
     orderBy: {
       likeCount: "desc",
@@ -14,5 +17,35 @@ export default async function handler(
   });
   const bestStories = allStories.slice(0, 5);
 
-  return res.status(200).json(bestStories);
+  const isLiked = [];
+  const allStoryIdForUserLike = [];
+
+  // 1. 비로그인 유저
+  if (!userId) {
+    bestStories.forEach((value: any) => {
+      isLiked.push(Object.assign(value, { liked: false }));
+    });
+  }
+  // 2. 로그인 유저
+  else {
+    const allStoryLikeForUser = await prisma.storyLike.findMany({
+      where: { userId },
+      include: {
+        story: true,
+      },
+    });
+    allStoryLikeForUser.forEach((value: any) => {
+      allStoryIdForUserLike.push(value.story.id);
+    });
+    bestStories.forEach((value: any) => {
+      if (allStoryIdForUserLike.includes(value.id)) {
+        console.log(true);
+        isLiked.push(Object.assign(value, { liked: true }));
+      } else {
+        isLiked.push(Object.assign(value, { liked: false }));
+      }
+    });
+  }
+
+  return res.status(200).json(isLiked);
 }
