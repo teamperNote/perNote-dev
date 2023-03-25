4228f196f24fdcc8be0b57183ee3508a0a78d05a
somin <smhs12@naver.com>
2023-03-25 16:21:35 +0900

    [fix] 디버깅 에러 임시 해결

--- components/KakaoShare.tsx
+++ components/KakaoShare.tsx
@@ -2,52 +2,47 @@ import React from "react";
 import styled from "styled-components";
 
 function KakaoShare() {
-  // const onClick = () => {
-  //   const { Kakao, location } = window;
-  //   Kakao.Share.createDefaultButton({
-  //     container: "#kakao-button",
-  //     objectType: "feed",
-  //     content: {
-  //       title: "pernote",
-  //       description: "오소민이 했숴여!!!!!!",
-  //       imageUrl: "IMAGE_URL",
-  //       link: {
-  //         webUrl: location.href,
-  //       },
-  //     },
-  //     social: {
-  //       likeCount: 1,
-  //       commentCount: 2,
-  //       sharedCount: 3,
-  //       viewCount: 10000,
-  //       subscriberCount: 1231241,
-  //     },
-  //     buttons: [
-  //       {
-  //         title: "웹으로 이동",
-  //         link: {
-  //           mobileWebUrl: "https://developers.kakao.com",
-  //           webUrl: "https://developers.kakao.com",
-  //         },
-  //       },
-  //       {
-  //         title: "앱으로 이동",
-  //         link: {
-  //           mobileWebUrl: "https://developers.kakao.com",
-  //           webUrl: "https://developers.kakao.com",
-  //         },
-  //       },
-  //     ],
-  //   });
-  // };
+  const onClick = () => {
+    const { Kakao, location } = window;
+    Kakao.Share.createDefaultButton({
+      container: "#kakao-button",
+      objectType: "feed",
+      content: {
+        title: "pernote",
+        description: "오소민이 했숴여!!!!!!",
+        imageUrl: "IMAGE_URL",
+        link: {
+          webUrl: location.href,
+        },
+      },
+      social: {
+        likeCount: 1,
+        commentCount: 2,
+        sharedCount: 3,
+        viewCount: 10000,
+        subscriberCount: 1231241,
+      },
+      buttons: [
+        {
+          title: "웹으로 이동",
+          link: {
+            mobileWebUrl: "https://developers.kakao.com",
+            webUrl: "https://developers.kakao.com",
+          },
+        },
+        {
+          title: "앱으로 이동",
+          link: {
+            mobileWebUrl: "https://developers.kakao.com",
+            webUrl: "https://developers.kakao.com",
+          },
+        },
+      ],
+    });
+  };
   return (
     <>
-      <button
-        id="kakao-button"
-        onClick={() => {
-          console.log("share");
-        }}
-      >
+      <button id="kakao-button" onClick={onClick}>
         카카오 공유하기
       </button>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import Layout from "../components/layout/Layout";
 import { GlobalStyle } from "../styles/global-styles";
 import "../styles/common.scss";
 import { RecoilRoot } from "recoil";
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
