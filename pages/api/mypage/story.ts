959d22e76cbbcb948aed9f880e243bf0e9fe644d
kjyong702 <kjyong702@naver.com>
2023-04-04 15:56:55 +0900

    feat: 마이페이지-찜한 스토리 api 추가

---
+++ pages/api/mypage/story.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+import { jwtVerify } from "jose";
+
+const secretKey = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const role = req.headers.authorization;
+
+  const accessToken = role.split("Bearer ")[1];
+  const { payload } = await jwtVerify(accessToken, secretKey);
+
+  const userId = payload.iss;
+
+  const allStoriesForUser = [];
+
+  const allStoryLikesForUser = await prisma.storyLike.findMany({
+    where: { userId },
+    include: {
+      story: true,
+    },
+  });
+  allStoryLikesForUser.forEach((value: any) => {
+    allStoriesForUser.push(Object.assign(value.story, { liked: true }));
+  });
+
+  return res.status(200).json(allStoriesForUser);
+}
