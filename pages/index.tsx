bf41b959add3fb6bbb99b8c9ca2fc5952ef39e42
somin <smhs12@naver.com>
2023-04-05 23:11:39 +0900

    Style: 메인페이지 이미지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,25 @@ import type { NextPage } from "next";
 import styled from "styled-components";
 import { BsBell } from "react-icons/bs";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
       <FirstMain>
         <MainText>
-          <MainTitle>메인화면</MainTitle>
-          <MainContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa
-            <br />
-            gravida sit facilisis cras. In blandit mi id est luctus. Non turpis
-            non
-            <br /> risus purus. Eget vel commodo ac purus, laoreet sollicitudin.
-          </MainContent>
+          <MainTitle>
+            <Image src="/main_logo.png" alt="per per" width={260} height={60} />
+          </MainTitle>
+          <MainContent>향수 여정의 시작을 편하게</MainContent>
         </MainText>
       </FirstMain>
       <SecondMain>
-        <PerNoteImage src="/perNoteBackImg.png" alt="second main image" />
+        <PerNoteImage src="/main_intro.png" alt="second main image" />
         <PerNoteIntroContent>
           {`향기는 첫인상이죠. 무언의 감각이며 단어가 없는 언어입니다. 자신에게 어울리는, 자신이 원하는 향수를
-           찾고 싶지만 향수를 맡아보러 매장까지 가는 것도 쉽지 않은 일입니다.
-      언제 어디서든 향수를 맡아보지 않더라도 쉽게 비교하고 선택도록 함께 하겠습니다.
+                            찾고 싶지만 향수를 맡아보러 매장까지 가는 것도 쉽지 않은 일입니다.
+               언제 어디서든 향수를 맡아보지 않더라도 쉽게 비교하고 선택도록 함께 하겠습니다.
           `}
         </PerNoteIntroContent>
       </SecondMain>
@@ -34,7 +31,7 @@ const Home: NextPage = () => {
           <ScentContent>
             {`      향수를 모르더라도 걱정하지 마세요.
 계절, 색깔, 자신의 성격 등의 간단한 설문조사로 
-        당신의 향수를 찾아드려요.
+              당신의 향수를 찾아드려요.
               `}
           </ScentContent>
           <ScentButton>
@@ -46,15 +43,15 @@ const Home: NextPage = () => {
         <FourthFirst>
           <PerfumeList>
             <PerfumeItem>
-              <PerfumeImage src="/perNoteBackImg.png" alt="image" />
+              <PerfumeImage src="/main_story1.png" alt="image" />
               <PerfumeName>Lorem Ipsum</PerfumeName>
             </PerfumeItem>
             <PerfumeItem>
-              <PerfumeImage src="/perNoteBackImg.png" alt="image" />
+              <PerfumeImage src="/main_story2.png" alt="image" />
               <PerfumeName>Lorem Ipsum</PerfumeName>
             </PerfumeItem>
             <PerfumeItem>
-              <PerfumeImage src="/perNoteBackImg.png" alt="image" />
+              <PerfumeImage src="/main_story3.png" alt="image" />
               <PerfumeName>Lorem Ipsum</PerfumeName>
             </PerfumeItem>
           </PerfumeList>
@@ -84,8 +81,8 @@ const Home: NextPage = () => {
             </CategoryButton>
           </CategoryIntro>
           <div>
-            <CategoryImage src="/perNoteBackImg.png" alt="image1" />
-            <CategoryImage src="/perNoteBackImg.png" alt="image2" />
+            <CategoryImage src="/main_category1.png" alt="image1" />
+            <CategoryImage src="/main_category2.png" alt="image2" />
           </div>
         </FourthSecond>
       </FourthMain>
@@ -99,7 +96,8 @@ const FirstMain = styled.div`
   position: relative;
   color: white;
   height: 1080px;
-  background-image: url("/perNoteBackImg.png");
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("/main_back.png");
   background-size: cover;
   @media screen and (max-width: 1440px) {
     height: 789px;
@@ -110,19 +108,20 @@ const MainText = styled.div`
   width: 921px;
   margin-left: 174px;
   position: absolute;
-  bottom: 102px;
+  bottom: 210px;
   left: 0;
   z-index: 1;
+  @media screen and (max-width: 1440px) {
+    bottom: 102px;
+  }
 `;
 const MainTitle = styled.div`
   margin-bottom: 30px;
-  font-weight: bold;
-  font-size: 3.125rem;
 `;
 
 const MainContent = styled.div`
   font-weight: normal;
-  font-size: 1.875rem;
+  font-size: 2.5rem;
 `;
 const SecondMain = styled.div`
   margin-top: 196px;
@@ -152,12 +151,14 @@ const PerNoteIntroContent = styled.pre`
   line-height: 1.6;
   margin: 0;
   /* 메인 두번째 자간 설정 */
-  letter-spacing: -2px;
+  letter-spacing: -1px;
 `;
 
 // 세번째
 const ThirdMain = styled.div`
-  background: url("/perNoteBackImg.png");
+  background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
+    url("/main_personal.png");
+  background-size: cover;
   width: 100%;
   height: 1080px;
   margin-bottom: 200px;
