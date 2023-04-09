24f09e3fbd37a6d7398471fa02a4f6a9b7eb47db
kjyong702 <kjyong702@naver.com>
2023-04-10 05:15:17 +0900

    feat: 마이페이지-테스트결과 api 추가

---
+++ pages/api/mypage/test.ts
@@ -0,0 +1,76 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { jwtVerify } from "jose";
+import prisma from "../../../prisma/client";
+
+const secretKey = new TextEncoder().encode(process.env.JWT_SECRET_KEY);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const role = req.headers.authorization;
+  const accessToken = role.split("Bearer ")[1];
+  const { payload } = await jwtVerify(accessToken, secretKey);
+
+  const id = payload.iss;
+
+  // Initiate response data
+  let resStatus, resData;
+
+  const test = await prisma.test.findMany({
+    where: {
+      userId: id,
+    },
+    select: {
+      id: true,
+      createdAt: true,
+      perfumeIDs: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  if (!test) {
+    resStatus = 200;
+    resData = { message: "Error: personalScent/result" };
+  } else {
+    for (let i = 0; i < test.length; i++) {
+      const top1 = await prisma.perfume.findMany({
+        where: {
+          id: test[i].perfumeIDs[0],
+        },
+        select: {
+          name_eng: true,
+          brand_eng: true,
+          top: true,
+          middle: true,
+          bottom: true,
+          imgUrl: true,
+        },
+      });
+
+      const perfumeNote = await prisma.perfume_CategoryInfo.findMany({
+        where: {
+          name_eng: top1[0].name_eng,
+        },
+        select: {
+          note: true,
+        },
+      });
+
+      test[i]["note"] = perfumeNote[0].note;
+
+      for (const [key, val] of Object.entries(top1[0])) {
+        test[i][key] = val;
+      }
+
+      delete test[i].perfumeIDs;
+    }
+
+    resStatus = 200;
+    resData = test;
+  }
+
+  return res.status(resStatus).send(resData);
+}
