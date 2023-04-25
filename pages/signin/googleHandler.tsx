b88b9c3345632344748d4d22f0a18241d6d83c43
somin <smhs12@naver.com>
2023-04-25 18:28:15 +0900

    Feat: sns 회원가입 기능 추가

--- pages/signin/googleHandler.tsx
+++ pages/signin/googleHandler.tsx
@@ -1,27 +1,39 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-
+import { Cookies } from "react-cookie";
 import React, { useEffect } from "react";
-
+import { useRecoilState } from "recoil";
+import { loginState } from "@store/loginState";
 function GoogleHandler() {
+  const cookies = new Cookies();
   const router = useRouter();
+  const [loginInfo, setLoginInfo] = useRecoilState(loginState);
 
   useEffect(() => {
     const parsedHash = new URLSearchParams(window.location.hash.substring(1));
     const token = parsedHash.get("access_token");
+
     const data = {
       access_token: token,
     };
     axios.post("/api/auth/google/login", data).then((res) => {
-      console.log(res);
       if (res.data.message === "가입되지 않은 사용자입니다") {
         router.push(
           "/sns-signup/google",
           `/sns-signup/google/${res.data.userId}`,
         );
       }
+      const { user, accessToken, refreshToken } = res.data;
+      localStorage.setItem("accessToken", accessToken);
+      cookies.set("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: "none",
+      });
+      setLoginInfo(user.email);
+      router.push("/");
     });
-  }, [router]);
+  }, [router, setLoginInfo]);
   return <div></div>;
 }
 export default GoogleHandler;
--- pages/signin/kakaoHandler.tsx
+++ pages/signin/kakaoHandler.tsx
@@ -1,23 +1,42 @@
 import axios from "axios";
 import { useRouter } from "next/router";
+import { Cookies } from "react-cookie";
 import React, { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { loginState } from "@store/loginState";
 
 function KakaoHandler(props: any) {
+  const cookies = new Cookies();
   const router = useRouter();
+  const [loginInfo, setLoginInfo] = useRecoilState(loginState);
+
   useEffect(() => {
     const data = {
       code: props.code,
     };
-    axios.post("/api/auth/kakao/login", data).then((res) => {
-      console.log(res);
-      if (res.data.message === "가입되지 않은 사용자입니다") {
-        router.push(
-          "/sns-signup/kakao",
-          `/sns-signup/kakao/${res.data.userId}`,
-        );
-      }
-    });
-  }, [props.code, router]);
+    axios
+      .post("/api/auth/kakao/login", data)
+      .then((res) => {
+        if (res.data.message === "가입되지 않은 사용자입니다") {
+          router.push(
+            "/sns-signup/kakao",
+            `/sns-signup/kakao/${res.data.userId}`,
+          );
+        }
+        const { user, accessToken, refreshToken } = res.data;
+        localStorage.setItem("accessToken", accessToken);
+        cookies.set("refreshToken", refreshToken, {
+          path: "/",
+          secure: true,
+          sameSite: "none",
+        });
+        setLoginInfo(user.email);
+        router.push("/");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [props.code, router, setLoginInfo]);
   return <div></div>;
 }
 
